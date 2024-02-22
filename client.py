c7e11caa32fd259ca8d03ab53460f603ff1dab1a
StanleyIG <oootehts@gmail.com>
2024-02-22 22:12:07 +0300

    fix code

---
+++ client.py
@@ -0,0 +1,114 @@
+import requests
+import asyncio
+import aiohttp
+import random
+import time
+import json
+from contextvars import ContextVar
+
+
+# Создание контекстной переменной для глобального доступа
+TextsList = ContextVar('texts_list', default=[])
+
+
+async def get_fish_text(num: int) -> None:
+    """ Запрос и обработка Рыба-текста из 3 абзацев, полученного в json.
+    Получение данных с помощью aiohttp и присвоение в контекстную переменную.
+    """
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                'https://fish-text.ru/get?format=json&number=3') as resp:
+            json_data = await resp.text()
+            dict_data = json.loads(json_data)
+            status = dict_data['status']
+            text = dict_data['text']
+            texts_list = TextsList.get()
+            if status == 'success':
+                texts_list.append(text[:100])
+            else:
+                texts_list.append(status)
+            TextsList.set(texts_list)
+
+
+async def main(quantity: int = 33) -> None:
+    """Создание асинхронных задач и обработка результатов выполнения"""
+    await asyncio.gather(*(get_fish_text(num) for num in range(1, quantity)))
+
+
+def get_fish_texts() -> list:
+    """ Получить список из объектов с Рыба-текстами
+    Обнуление данных в контекстной переменной.
+    Асинхронное получение новых данных с помощью asyncio и aiohttp.
+    Каждый раз возвращается новый список с новыми данными
+    """
+    texts_list = TextsList.get()
+    texts_list.clear()
+
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    event_loop.run_until_complete(main(10))
+
+    texts_list = TextsList.get()
+    return texts_list
+
+texts = get_fish_texts()
+
+#print(random.choice(texts))
+
+
+
+
+# url = 'http://127.0.0.1:5003/add-message'
+# params = {
+#     'name': 'gergre',
+#     'text': 'sdkjgfdsgjfdpg'
+# }
+
+# response = requests.post(url, params=params)
+
+# if response.status_code == 200:
+#     print(response.json())
+# else:
+#     print(f'ошибка: {response.status_code}')
+
+
+
+SERVER_URLS = ['http://127.0.0.1:5001/add-message', 'http://127.0.0.1:5002/add-message', 'http://127.0.0.1:5003/add-message']
+USERS = ['Александр', 'Анна', 'Мария', 'Иван', 'Екатерина', 'Дмитрий', 'Ольга', 'Павел', 'София', 'Николай']
+
+
+async def get_resp(url, session, user, texts):
+    try:
+        async with session.post(url, params={'name': user, 'text': random.choice(texts)}) as response:
+            response.raise_for_status()  # Проверка на успешный статус ответа
+            return await response.text()
+    except aiohttp.ClientResponseError as e:
+        pass
+        print(f'Ошибка при отправке запроса: {e}')
+
+
+async def send_requests(texts):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for _ in range(50):
+            user = random.choice(USERS)
+            for _ in range(100):
+                url = random.choice(SERVER_URLS)
+                task = asyncio.create_task(get_resp(url, session, user, texts))
+                tasks.append(task)
+
+        start_time = time.time()
+        await asyncio.gather(*tasks)
+        end_time = time.time()
+
+        total_time = end_time - start_time
+        time_per_request = total_time / 5000
+        throughput = 5000 / total_time
+
+        print(f'Общее время выполнения 5000 запросов: {total_time} секунд')
+        print(f'Время на запрос: {time_per_request} секунд')
+        print(f'Пропускная способность: {throughput} запросов в секунду')
+
+if __name__ == '__main__':
+    texts = get_fish_texts()
+    asyncio.run(send_requests(texts))
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import asyncio
 import uvicorn
 from fastapi.requests import Request
 from typing import Optional, Annotated
@@ -6,12 +7,14 @@ from fastapi import Depends
 from pydantic import BaseModel, ConfigDict
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr, declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from fastapi.responses import JSONResponse
 from contextlib import asynccontextmanager
 from sqlalchemy import desc
 from sqlalchemy.future import select
 import datetime
+from uvicorn import Server, Config
+from aioredis import client
 
 
 message_route = APIRouter()
@@ -53,7 +56,7 @@ class Base(DeclarativeBase):
 class Messages(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    text = Column(String, nullable=True)
+    text = Column(String(1000), nullable=True)
     date = Column(DateTime, default=datetime.datetime.utcnow)
 
 
@@ -106,19 +109,48 @@ async def add_message(user_post: Annotated[UserBodyRequestToDB, Depends()]):
 
 
 # функция выполняет процесс создания приложения и запуск зависимостей, в частности создание базы
+app_ports = [5001, 5002, 5003]  # Порты, на которых будут запущены сервера
+
+
 def create_app():
+    """создание приложения, подключение роута(ов) и запуск зависимостей"""
     @asynccontextmanager
     async def lifespan(app: FastAPI):
-        """При запуске сервера создаётся база, если базы ещё не не существует"""
-        await create_db()
+        """При запуске сервера создаётся база, если базы ещё не существует"""
+        try:
+            await create_db()
+        except:
+            pass
         yield
+
     app = FastAPI(lifespan=lifespan)
     app.include_router(message_route)
 
     return app
 
 
-# Запуск сервера FastAPI
 app = create_app()
+
+
+# сылка на источник https://gist.github.com/tenuki/ff67f87cba5c4c04fd08d9c800437477
+
+class MyServer(Server):
+    async def run(self, sockets=None):
+        self.config.setup_event_loop()
+        return await self.serve(sockets=sockets)
+
+
+async def run():
+    apps = []
+    for cfg in app_ports:
+        config = Config("server:app", host="127.0.0.1",
+                        port=cfg)
+        server = MyServer(config=config)
+        apps.append(server.run())
+    return await asyncio.gather(*apps)
+
+# сылка на источник https://gist.github.com/tenuki/ff67f87cba5c4c04fd08d9c800437477
+
 if __name__ == '__main__':
-    uvicorn.run('server:app', host='127.0.0.1', port=5000, reload=True)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
